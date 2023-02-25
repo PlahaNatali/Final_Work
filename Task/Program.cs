c6eb359b91d6c5ff29cdbf1a2969e5f7399a9fa7
Nataliya <krasiukovan@mail.ru>
2023-02-25 13:25:05 +0300

    Add Void Sorting Array

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,5 +1,20 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
 // длина которых меньше или ровна 3 символов.
 
-string[] array1 = new string[5] {"147", "hi", "7458", "work", "arr"};
-string[] array2 = new string[array1.Length];
+string[] array = new string[5] { "147", "hi", "7458", "work", "arr" };
+string[] arrayNew = new string[array.Length];
+
+SortingArray(array, arrayNew);
+
+void SortingArray(string[] array, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            array2[count] = array[i];
+            count++;
+        }
+    }
+}
