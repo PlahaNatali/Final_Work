797e80edc1c21932ab84634fabfc5e5259d01bd8
Nataliya <krasiukovan@mail.ru>
2023-02-25 13:56:42 +0300

    Correction Task

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,20 +1,20 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
 // длина которых меньше или ровна 3 символов.
 
-string[] array = new string[5] { "147", "hi", "7458", "work", "arr" };
+string[] array = new string[5] { "875", "hi", "me", "work", "arr" };
 string[] arrayNew = new string[array.Length];
 
 SortingArray(array, arrayNew);
 PrintArray(arrayNew);
 
-void SortingArray(string[] array, string[] array2)
+void SortingArray(string[] array, string[] arrayNew)
 {
     int count = 0;
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length <= 3)
         {
-            array2[count] = array[i];
+            arrayNew[count] = array[i];
             count++;
         }
     }
